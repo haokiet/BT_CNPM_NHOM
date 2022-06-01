005646e000adf97cf44a60536804d8f028edb445
haokiet <kietkhan0@gmail.com>
2022-06-01 09:58:03 +0700

    mới

--- QLRau.sql
+++ QLRau.sql
@@ -3,35 +3,35 @@ go
 use QLRau
 go
 create table NhanVien(	MaNV varchar(10) primary key,
-						TenDangNhap varchar(20) not null,
+						TenDangNhap varchar(50) not null,
 						MatKhau varchar(20) not null,
 						HoNV nvarchar(20) not null,
 						TenNV nvarchar(20) not null,
 						NgaySinh date ,
 						GioiTinh bit DEFAULT(1),
 						NgayLamViec date not null,
-						DiaChi nvarchar(50),
+						DiaChi nvarchar(100),
 						SoDT varchar(15),
 						Luong int)
 GO
 create table NCC(	MaNCC varchar(10) primary key,
-					TenNCC nvarchar(20) not null,
-					DiaChi nvarchar(50) not null,
-					Email varchar(50) not null,
+					TenNCC nvarchar(50) not null,
+					DiaChi nvarchar(100) not null,
+					Email varchar(100) not null,
 					SoDT varchar(15) )
 GO
 
 create table Loai(	MaLoai varchar(10) primary key,
-					TenLoai nvarchar(20) not null)
+					TenLoai nvarchar(50) not null)
 GO
 
 create table KhachHang(	MaKH varchar(10) primary key,
 						HoKH nvarchar(20) not null,
 						TenKH nvarchar(20) not null,
 						SoDT varchar(15) not null,
-						DiaChi nvarchar(50),
+						DiaChi nvarchar(100),
 						GioiTinh bit DEFAULT(1),
-						TenDN varchar(20) not null,
+						TenDN varchar(50) not null,
 						MatKhau varchar(20) not null)
 GO
 
@@ -39,7 +39,7 @@ create table HoaDon(	SoHD varchar(10) primary key,
 						MaKH varchar(10) references KhachHang(MaKH) not null,
 						MaNVDuyet varchar(10) references NhanVien(MaNV) not null,
 						MaNVGiaoHang varchar(10) references NhanVien(MaNV) not null,
-						DiaChiGiaoHang nvarchar(50) not null,
+						DiaChiGiaoHang nvarchar(100) not null,
 						TinhTrang nvarchar(20) not null,
 						NgayDatHang date not null,
 						NgayGiaoHang date)
@@ -48,9 +48,9 @@ GO
 create table SanPham(	MaSP varchar(10) primary key,
 						MaNCC varchar(10) references NCC(MaNCC) not null,
 						MaLoai varchar(10) references Loai(MaLoai) not null,
-						TenSP nvarchar(50) not null,
-						MoTaSP nvarchar(200),
-						AnhDaiDien  varchar(50),
+						TenSP nvarchar(100) not null,
+						MoTaSP nvarchar(500),
+						AnhDaiDien  Nvarchar(50),
 						DonGia int,
 						DonViTinh nvarchar(10))
 GO
@@ -61,7 +61,7 @@ create table CTHoaDon(	SoHD varchar(10) references HoaDon(SoHD),
 						DonGia int)
 GO
 create table ThamSo(	MaTS varchar(10) primary key,
-						TenTS nvarchar(20) not null,
+						TenTS nvarchar(100) not null,
 						DonViTinh nvarchar(20),
 						GiaTri int,
 						TinhTrang bit DEFAULT(1))
@@ -74,10 +74,10 @@ VALUES ('NV01', 'khuevotan','123', N'Võ', N'Tấn Khuê','0987664220', N'Cam Ra
 	   ('NV02', 'admin','123', N'Quản', N'Trị Viên','01627240041', N'Tuy Hòa, Phú Yên', CAST(N'2020-09-06' AS Date), 200000 ,CAST(N'2001-09-06' AS Date), 1 ),
 	   ('NV03', 'huynguyenhuu','123', N'Nguyễn', N'Hữu Huy','0987413571', N'Cam Lâm, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-03-04' AS Date), 1),
 	   ('NV04', 'phucnguyenvan','123', N'Nguyễn', N'Văn Phúc','0983468912', N'Nha Trang, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-11-24' AS Date), 1),
-	   ('NV05', 'phuongthihuynh','123', N'Huỳnh', N'Thị Phương','0983347511', N'Đông Hòa, Phú Yên',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-02-07' AS Date), 2),
+	   ('NV05', 'phuongthihuynh','123', N'Huỳnh', N'Thị Phương','0983347511', N'Đông Hòa, Phú Yên',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-02-07' AS Date), 0),
 	   ('NV06', 'khanhtranvan','123', N'Trần', N'Văn Khánh','0984478901', N'Nha Trang, Khánh Hòa',CAST(N'2021-09-06' AS Date),200000 ,CAST(N'2001-06-24' AS Date), 1),
 	   ('NV07', 'namnguyenvan','123', N'Nguyễn', N'Văn Nam','0984416745', N'Nha Trang, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-09-22' AS Date), 1),
-	   ('NV08', 'nhinguyenthiai','123', N'Nguyễn', N'Thị Ái Nhi','0983468912', N'Cam Lâm, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-11-03' AS Date), 2),
+	   ('NV08', 'nhinguyenthiai','123', N'Nguyễn', N'Thị Ái Nhi','0983468912', N'Cam Lâm, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-11-03' AS Date), 0),
 	   ('NV09', 'huytranvan','123', N'Trần', N'Văn Huy','0983515131', N'Nha Trang, Khánh Hòa',CAST(N'2021-09-06' AS Date),100000 ,CAST(N'2001-10-01' AS Date), 1),
 	   ('NV10', 'datnguyen','123', N'Nguyễn', N'Thành Đạt','0984141674', N'Nha Trang, Khánh Hòa',CAST(N'2021-09-06' AS Date),200000 ,CAST(N'2001-11-20' AS Date), 1)
 
@@ -122,26 +122,26 @@ VALUES	('KH001', N'huynguyen','123', N'Nguyễn', N'Huy',  0985101312, N'Cam Ran
 		('KH010', N'thupham','123', N'Phạm', N'Thu', 0985441312, N'Cam Lâm, Khánh Hòa',  0)
 GO
 INSERT INTO dbo.HoaDon (SoHD , NgayDatHang, NgayGiaoHang, DiaChiGiaoHang,  TinhTrang, MaNVDuyet, MaNVGiaoHang, MaKH)
-VALUES	('HD001', CAST(N'2020-09-03' AS Date), CAST(N'2020-09-04' AS Date), N'Vĩnh Điềm Trung', 'Duyệt','NV01', 'NV01','KH001'),
-		('HD002', CAST(N'2020-10-02' AS Date), CAST(N'2020-10-03' AS Date),  N'Mã Vòng',  'Duyệt', 'NV03','NV01','KH002'),
-		('HD003', CAST(N'2020-04-10' AS Date), CAST(N'2020-04-11' AS Date),	N'Vĩnh Điềm Trung', 'Duyệt','NV01', 'NV01','KH003'),
-		('HD004', CAST(N'2020-09-11' AS Date), CAST(N'2020-09-12' AS Date),  N'Vĩnh Hải', 'Duyệt', 'NV02','NV01', 'KH004'),
-		('HD005', CAST(N'2020-04-06' AS Date), CAST(N'2020-04-07' AS Date),  N'Vĩnh Phước',  'Duyệt', 'NV08','NV01', 'KH005'),
-		('HD006', CAST(N'2020-11-20' AS Date), CAST(N'2020-11-21' AS Date),  N'Xóm Cồn', 'Duyệt', 'NV05','NV01', 'KH006'),
-		('HD007', CAST(N'2020-10-11' AS Date), CAST(N'2020-10-12' AS Date),  N'Vĩnh Hải',  'Duyệt', 'NV10','NV01', 'KH007'),
-		('HD008', CAST(N'2020-02-01' AS Date), CAST(N'2020-02-02' AS Date),  N'Vĩnh Phước',  'Duyệt', 'NV04','NV01', 'KH008'),
-		('HD009', CAST(N'2020-01-03' AS Date), CAST(N'2020-01-04' AS Date), N'Mường Thanh',  'Duyệt', 'NV09', 'NV01','KH009'),
-		('HD010', CAST(N'2020-11-09' AS Date), CAST(N'2020-11-10' AS Date),  N'Vĩnh Điềm Trung',  'Duyệt', 'NV01','NV03', 'KH001')
+VALUES	('HD001', CAST(N'2020-09-03' AS Date), CAST(N'2020-09-04' AS Date), N'Vĩnh Điềm Trung', N'Duyệt','NV01', 'NV01','KH001'),
+		('HD002', CAST(N'2020-10-02' AS Date), CAST(N'2020-10-03' AS Date),  N'Mã Vòng',  N'Duyệt', 'NV03','NV01','KH002'),
+		('HD003', CAST(N'2020-04-10' AS Date), CAST(N'2020-04-11' AS Date),	N'Vĩnh Điềm Trung', N'Duyệt','NV01', 'NV01','KH003'),
+		('HD004', CAST(N'2020-09-11' AS Date), CAST(N'2020-09-12' AS Date),  N'Vĩnh Hải', N'Duyệt', 'NV02','NV01', 'KH004'),
+		('HD005', CAST(N'2020-04-06' AS Date), CAST(N'2020-04-07' AS Date),  N'Vĩnh Phước',  N'Duyệt', 'NV08','NV01', 'KH005'),
+		('HD006', CAST(N'2020-11-20' AS Date), CAST(N'2020-11-21' AS Date),  N'Xóm Cồn', N'Duyệt', 'NV05','NV01', 'KH006'),
+		('HD007', CAST(N'2020-10-11' AS Date), CAST(N'2020-10-12' AS Date),  N'Vĩnh Hải',  N'Duyệt', 'NV10','NV01', 'KH007'),
+		('HD008', CAST(N'2020-02-01' AS Date), CAST(N'2020-02-02' AS Date),  N'Vĩnh Phước',  N'Duyệt', 'NV04','NV01', 'KH008'),
+		('HD009', CAST(N'2020-01-03' AS Date), CAST(N'2020-01-04' AS Date), N'Mường Thanh',  N'Duyệt', 'NV09', 'NV01','KH009'),
+		('HD010', CAST(N'2020-11-09' AS Date), CAST(N'2020-11-10' AS Date),  N'Vĩnh Điềm Trung',  N'Duyệt', 'NV01','NV03', 'KH001')
 GO
 
 
 GO
 INSERT INTO dbo.SanPham (MaSP, TenSP, AnhDaiDien,MoTaSP, DonGia, MaNCC, MaLoai,DonViTinh)
-VALUES	('SP01', N'Nấm kim châm', 'namkimcham.jpg',  N'Nấm kim châm Hàn Quốc được nuôi trồng và đóng gói theo những tiêu chuẩn nghiêm ngặt', 10000, 'NCC01','DM01','VND'),
-		('SP02', N'Cải ngọt', 'caingot.jpg',N'Rau an toàn 4KFarm với tiêu chí 4 KHÔNG, luôn ưu tiên bảo vệ sức khỏe người tiêu dùng', 15000, 'NCC02','DM01','VND'),
-		('SP03', N'Rau muống', 'raumuong.jpg', N'Rau an toàn 4KFarm với tiêu chí 4 KHÔNG, luôn ưu tiên bảo vệ sức khỏe người tiêu dùng', 9000 , 'NCC03', 'DM01','VND'),
-		('SP04', N'Cà rốt ', 'carot.jpg',  N'Cà rốt Đà Lạt là một loại củ rất quen thuộc trong các món ăn của người Việt. Loại củ này ', 5000 , 'NCC05', 'DM02','VND'),
-		('SP05', N'Bắp mỹ', 'bap.jpg',  N'Bắp Mỹ là một loại thực phẩm được trồng rất nhiều ở khắp nơi trên thế giới. Đây là một loại ', 15000,'NCC04', 'DM02','VND')	
+VALUES	('SP01', N'Nấm kim châm', N'namkimcham.jpg',  N'Nấm kim châm Hàn Quốc được nuôi trồng và đóng gói theo những tiêu chuẩn nghiêm ngặt', 10000, 'NCC01','DM01','VND'),
+		('SP02', N'Cải ngọt', N'caingot.jpg',N'Rau an toàn 4KFarm với tiêu chí 4 KHÔNG, luôn ưu tiên bảo vệ sức khỏe người tiêu dùng', 15000, 'NCC02','DM01','VND'),
+		('SP03', N'Rau muống', N'raumuong.jpg', N'Rau an toàn 4KFarm với tiêu chí 4 KHÔNG, luôn ưu tiên bảo vệ sức khỏe người tiêu dùng', 9000 , 'NCC03', 'DM01','VND'),
+		('SP04', N'Cà rốt ', N'carot.jpg',  N'Cà rốt Đà Lạt là một loại củ rất quen thuộc trong các món ăn của người Việt. Loại củ này ', 5000 , 'NCC05', 'DM02','VND'),
+		('SP05', N'Bắp mỹ', N'bap.jpg',  N'Bắp Mỹ là một loại thực phẩm được trồng rất nhiều ở khắp nơi trên thế giới. Đây là một loại ', 15000,'NCC04', 'DM02','VND')	
 GO
 
 
