6f04fa42f02b2cf202979147c8944f943f87b213
haokiet <kietkhan0@gmail.com>
2022-06-06 10:50:56 +0700

    mo

---
+++ BTNhom3_CNPM/BTNhom3_CNPM/Controllers/NCCsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTNhom3_CNPM.Models;
+
+namespace BTNhom3_CNPM.Controllers
+{
+    public class NCCsController : Controller
+    {
+        private QLRauEntities db = new QLRauEntities();
+
+        // GET: NCCs
+        public ActionResult Index()
+        {
+            return View(db.NCCs.ToList());
+        }
+
+        // GET: NCCs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NCC nCC = db.NCCs.Find(id);
+            if (nCC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nCC);
+        }
+
+        // GET: NCCs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NCCs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNCC,TenNCC,DiaChi,Email,SoDT")] NCC nCC)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NCCs.Add(nCC);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nCC);
+        }
+
+        // GET: NCCs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NCC nCC = db.NCCs.Find(id);
+            if (nCC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nCC);
+        }
+
+        // POST: NCCs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNCC,TenNCC,DiaChi,Email,SoDT")] NCC nCC)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nCC).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nCC);
+        }
+
+        // GET: NCCs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NCC nCC = db.NCCs.Find(id);
+            if (nCC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nCC);
+        }
+
+        // POST: NCCs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NCC nCC = db.NCCs.Find(id);
+            db.NCCs.Remove(nCC);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
